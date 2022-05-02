bfb2e9c2eaf56f09bc4551be252b0c90b1780963
ediazjz <iam.ediazjz@gmail.com>
2022-05-01 20:21:27 -0500

    config: Add build configuration for Vite

--- postcss.config.js
+++ postcss.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   plugins: [
-    'tailwindcss/nesting',
-    'tailwindcss',
-    'autoprefixer',
-  ],
+    require("tailwindcss"),
+    require("tailwindcss/nesting"),
+    require("autoprefixer"),
+  ]
 }
