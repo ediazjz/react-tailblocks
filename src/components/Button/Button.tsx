d1da025eb0e5fdccc07c636b4bb46fbc610c0cc6
ediazjz <iam.ediazjz@gmail.com>
2022-05-01 19:47:47 -0500

    feat: Remove biolerplate componets and stories

---
+++ src/components/Button/Button.tsx
@@ -0,0 +1,11 @@
+interface ButtonProps {
+  label: string;
+}
+
+const Button = ({ label }: ButtonProps ) => {
+  return (
+    <button className="text-white bg-indigo-500 border-0 py-2 px-8 focus:outline-none hover:bg-indigo-600 rounded text-lg">{label}</button>
+  )
+}
+ 
+export default Button
