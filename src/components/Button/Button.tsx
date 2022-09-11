5d0649a4e8d266a50cfb5a522b054280e221882b
ediazjz <iam.ediazjz@gmail.com>
2022-09-10 22:44:48 -0500

    feat: add mutable button

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,13 +1,30 @@
 import "../../tailwind.css"
 
-interface ButtonProps {
+type ButtonOwnProps<E extends React.ElementType = React.ElementType> = {
   label: string
+  as?: E
 }
 
-const Button = ({ label }: ButtonProps ) => {
+type ButtonProps<E extends React.ElementType> = ButtonOwnProps<E> &
+  Omit<React.ComponentProps<E>, keyof ButtonOwnProps>
+
+const __DEFAULT_ELEMENT__ = 'button'
+
+const Button = <E extends React.ElementType = typeof __DEFAULT_ELEMENT__> ({
+  label,
+  as,
+  ...props
+}: ButtonProps<E>) => {
+  const Component = as || __DEFAULT_ELEMENT__
+
   return (
-    <button className="text-white bg-indigo-500 border-0 py-2 px-8 focus:outline-none hover:bg-indigo-600 rounded text-lg">{label}</button>
+    <Component
+      {...props}
+      className="text-white bg-indigo-500 border-0 py-2 px-8 focus:outline-none hover:bg-indigo-600 rounded text-lg"
+    >
+      {label}
+    </Component>
   )
 }
- 
+
 export default Button
